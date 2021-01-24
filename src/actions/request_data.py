9bbee67f628ecbd9f808e6f3675920dde5fe8761
jmaralc <jmaralc@gmail.com>
2021-01-24 18:17:41 +0100

    latest data version downloaded

---
+++ src/actions/request_data.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+url = "https://api.corvusbelli.com/army/units/es/{faction_id}"
+root_assets = "./assets"
+faction_file = "{assets_path}/faction_{faction_id}.json"
+metadata_file = "{assets_path}/metadata.json"
+
+
+def upsert_data():
+    with open(metadata_file.format(assets_path=root_assets),"r") as fid:
+        metadata = json.loads(fid.read())
+
+    factions =  metadata.get("factions")
+
+    for faction in factions:
+        faction_id = faction.get("id")
+        response = requests.get(url.format(faction_id=faction_id))
+        if response.status_code == 200:
+            if "Error" in str(response.content,"utf-8"):
+                continue
+            request_body = response.json()
+            if request_body is None:
+                continue
+        with open(faction_file.format(assets_path=root_assets,faction_id=faction_id), "w") as fid:
+            json.dump(request_body, fid)
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from actions.request_data import upsert_data
+
+if __name__ == "__main__":
+    upsert_data()
