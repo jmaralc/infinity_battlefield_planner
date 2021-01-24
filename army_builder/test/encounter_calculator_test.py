4cd9982246467eb17a87b5bd980c7494ecc29239
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-01-24 22:50:15 +0100

    Fixing broken test

--- army_builder/test/encounter_calculator_test.py
+++ army_builder/test/encounter_calculator_test.py
@@ -18,20 +18,12 @@ class TestEncounterCalculator:
     def test_calculate_hit_probabilities_for_one_shot_of_vanilla_BS13_unit_targeting_vanilla_unit_with_combi_in_good_range(self,):
         shooter = ArmyUnit(bs=13)
         shooter.weapon = CombiRifle()
-        encounter_context = {
+        
+        encounter = Encounter({
             "shooter": shooter,
             "distance": CombiRifle.GoodRange
-        }
-
-        result = compute_encounter(encounter_context)
+        })
+        
+        result = encounter.compute()
         
         assert result["hit"] == 0.8
-
-def compute_encounter(context):
-    shooter  = context["shooter"]
-    distance = context.get("distance")
-
-    hit         = shooter.threshold_hit(distance) /20.0
-    return {
-        "hit": hit,
-    }
