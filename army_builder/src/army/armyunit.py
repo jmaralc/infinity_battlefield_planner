512138cb010f28f382e0ff34d367f510dd96e791
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-01-24 21:05:18 +0100

    Moving classess to where they need to be and adding .env

--- army_builder/src/army/armyunit.py
+++ army_builder/src/army/armyunit.py
@@ -1,2 +1,20 @@
+
+
 class ArmyUnit:
-    pass
+    def __init__(
+        self, cc=0, bs=0, ph=0, wip=0, arm=0, bts=0, w=0, ava=0, points=0, scw=0
+    ):
+        self.cc = cc
+        self.bs = bs
+        self.ph = ph
+        self.wip = wip
+        self.arm = arm
+        self.bts = bts
+        self.w = w
+        self.ava = ava
+        self.points = points
+        self.scw = scw
+
+    def threshold_hit(self,):
+        return self.bs
+
