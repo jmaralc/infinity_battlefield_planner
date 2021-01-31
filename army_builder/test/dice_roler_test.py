997fb7048edacb36492a36d5887b57fea494299f
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-01-31 19:23:51 +0100

    Implementing rolls for 1 die

--- army_builder/test/dice_roler_test.py
+++ army_builder/test/dice_roler_test.py
@@ -1,6 +1,23 @@
-class TestDiceRoller:
+
+class DiceRoller:
+    def __init__(self):
+        self.dice_count = 1
+
+    def all_rolls(self):
+        return range(1, 21)
+
+
+class TestDiceRollerTest:
     def test_a_new_dice_roller_has_one_die(self,):
-        pass
-    
+        roller = DiceRoller()
+
+        assert roller.dice_count == 1
+
     def test_a_dice_roller_can_provide_all_dice_roll_combinations(self,):
-        pass
+        roller = DiceRoller()
+        
+        rolls = roller.all_rolls()
+
+        assert len(rolls) == 20
+        for (expected_value, value) in zip(range(1, 20), rolls):
+            assert expected_value == value
