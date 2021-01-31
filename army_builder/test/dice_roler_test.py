2a9d5caad662a3a77c0828026d498849e2088b9d
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-01-31 19:13:13 +0100

    Adding some new tests to guide the design

---
+++ army_builder/test/dice_roler_test.py
@@ -0,0 +1,6 @@
+class TestDiceRoller:
+    def test_a_new_dice_roller_has_one_die(self,):
+        pass
+    
+    def test_a_dice_roller_can_provide_all_dice_roll_combinations(self,):
+        pass
---
+++ army_builder/test/encounter_rules_test.py
@@ -0,0 +1,11 @@
+class TestEncounterRules:
+
+    def test_rule_returns_an_outcome(self,):
+        pass
+
+    def test_rule_needs_a_context_to_be_validated(self,):
+        pass
+
+    def test_rule_throws_exception_it_doenst_have_needed_params(self,):
+        pass
+
