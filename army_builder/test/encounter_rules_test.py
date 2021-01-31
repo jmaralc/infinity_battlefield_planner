1393b367fe64e8a7fa9114ac80bbdf5b9f3b4d51
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-01-31 20:53:22 +0100

    Adding some Rules scaffolding

--- army_builder/test/encounter_rules_test.py
+++ army_builder/test/encounter_rules_test.py
@@ -1,11 +1,54 @@
+from src.army.armyunit import UnitBuilder
+import pytest
+
+
+class Rule:
+    def __init__(self, needed_context_keys=[]):
+        self.__needed_context_keys = needed_context_keys
+
+    def apply(self, context={}):
+        self.__check_context_keys(context)
+        return True
+
+    def __check_context_keys(self, context):
+        keys = []
+        
+        for key in self.__needed_context_keys:
+            if key not in context:
+                keys.append(keys)
+    
+        if len(keys) > 0:         
+            message = "The following context keys are missing {}".format(keys)
+            raise Exception(message)
+
+
+class AttackHitsRule(Rule):
+    def __init__(self):
+        super().__init__(["shooter", "shooter_rolls", "target", "target_rolls"])
+    
+    def apply(self, context={}):
+        super().apply(context)
+        # Do some magic here
+        return True
+        
+
+
 class TestEncounterRules:
 
     def test_rule_returns_an_outcome(self,):
-        pass
+        rule = AttackHitsRule()
 
-    def test_rule_needs_a_context_to_be_validated(self,):
-        pass
+        outcome = rule.apply({
+            "shooter": UnitBuilder().build(),
+            "shooter_rolls": (10),
+            "target": UnitBuilder().build,
+            "target_rolls": (1)
+        })
+
+        assert outcome is not None
 
     def test_rule_throws_exception_it_doenst_have_needed_params(self,):
-        pass
+        with pytest.raises(Exception) as e_info:
+            rule = AttackHitsRule()
+            rule.apply()
 
